b1e4a7842e9dc6fae8d51cd6b9612003b4dafc0f
quocduong <quocduong0897@gmail.com>
2024-02-01 11:36:44 +0900

    init middlewares

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
-express = require('express');
+const morgan = require('morgan');
+const express = require('express');
+const { default: helmet } = require('helmet');
+const compression = require('compression');
 
 const app = express();
 
 //init middlewares
-
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
 //init db
 
 //init router
-
+app.get('/',(req,res,next)=>{
+    return res.status(200).json({
+        message:'Welcome ecommerce service'
+    });
+});
 //handling error
 
 module.exports = app;
